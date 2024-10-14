63931289a77b489906125ea16e1ad5795d905c66
MikeLippincott <1michaell2017@gmail.com>
2024-10-14 09:38:39 -0600

    fix the file path error

--- slurm_stats_check/get_slurm_stats.sh
+++ slurm_stats_check/get_slurm_stats.sh
@@ -27,11 +27,9 @@ jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*
 slurm_stats_files_dir="slurm_stats_files"
 # make the dir if it does not exist
 mkdir -p "$slurm_stats_files_dir"
-cd $slurm_stats_files_dir
 
-# slurm_stats files
-accounts_file="$(date +%Y-%m-%d_%H:%M:%S)_accounts_date.csv"
-jobs_file="$(date +%Y-%m-%d_%H:%M:%S)_jobs_date.csv"
+accounts_file="$slurm_stats_files_dir/$(date +%Y-%m-%d_%H:%M:%S)_accounts_date.csv"
+jobs_file="$slurm_stats_files_dir/$(date +%Y-%m-%d_%H:%M:%S)_jobs_date.csv"
 
 # Get the accounts and jobs data
 suacct amc-general "$days" > "$accounts_file"
