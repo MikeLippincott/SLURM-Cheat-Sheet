2dd2bc424f1166b7ac77d09e9bd3e56bce990753
MikeLippincott <1michaell2017@gmail.com>
2024-10-14 08:45:58 -0600

    change dir of slurm files

--- slurm_stats_check/get_slurm_stats.sh
+++ slurm_stats_check/get_slurm_stats.sh
@@ -14,8 +14,8 @@ module load anaconda
 
 conda init bash
 
-days=$1
-USER=$2
+days=30
+USER="$USER"
 n=25
 
 # load env
@@ -24,17 +24,20 @@ conda activate slurm_stats_env
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
 # stats files dir
-
+slurm_stats_files_dir="slurm_stats_files"
+# make the dir if it does not exist
+mkdir -p "$slurm_stats_files_dir"
+cd $slurm_stats_files_dir
 
 # slurm_stats files
-accounts_file="accounts_date_$(date +%Y-%m-%d_%H:%M:%S).csv"
-jobs_file="jobs_date_$(date +%Y-%m-%d_%H:%M:%S).csv"
+accounts_file="$(date +%Y-%m-%d_%H:%M:%S)_accounts_date.csv"
+jobs_file="$(date +%Y-%m-%d_%H:%M:%S)_jobs_date.csv"
 
 # Get the accounts and jobs data
 suacct amc-general "$days" > "$accounts_file"
 jobstats "$USER" "$days" > "$jobs_file"
 
-cd scripts 
+cd ../scripts
 
 # Run the python script
 python slurm_stats.py --acct "$accounts_file" --jobs_stats "$jobs_file" --days "$days" --user "$USER" --top_n "$n"
